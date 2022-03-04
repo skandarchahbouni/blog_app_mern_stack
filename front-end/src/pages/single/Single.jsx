36be72af7a80de70cb836da4957c22b36bec501b
unknown <js_chahbouni@esi.dz>
2022-03-04 16:51:19 +0100

    Single post

--- front-end/src/pages/single/Single.jsx
+++ front-end/src/pages/single/Single.jsx
@@ -1,11 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import Sidebar from "../../components/sidebar/Sidebar";
 import SinglePost from "../../components/singlePost/SinglePost";
 import "./single.css";
 
 export default function Single() {
+
+  const {pathname} = useLocation()
+  const id = pathname.split("/")[2]
+
+  const [post, setPost] = useState([])
+
+  useEffect(() => {
+    const getSinglePost = async ()=>{
+      let res;
+      try {
+        res = await axios.get(`/api/posts/${id}`)
+        setPost(res.data)
+      } catch (error) {
+        console.log(error.toJSON())
+      }
+    }
+    getSinglePost()
+  }, [id])
+  
   return (
     <div className="single">
-      <SinglePost />
+      <SinglePost post={post}/>
       <Sidebar />
     </div>
   );
